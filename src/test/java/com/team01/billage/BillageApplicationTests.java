d638b43ef1d1895a4ea6cb73ae51c65b8b229a7f
[CLOUD]나용진 <yjnaga@naver.com>
2024-11-19 14:43:36 +0900

    create project

---
+++ src/test/java/com/team01/billage/BillageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team01.billage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
