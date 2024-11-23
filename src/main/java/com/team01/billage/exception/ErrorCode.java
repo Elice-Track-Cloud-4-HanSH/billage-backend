2c7778c6f6dfdc73cff0c1e8ffeffb534ba372c4
[CLOUD4] 허수빈 <anmedi3623@naver.com>
2024-11-23 14:14:33 +0900

    feat: 공통 예외 처리

---
+++ src/main/java/com/team01/billage/exception/ErrorCode.java
@@ -0,0 +1,29 @@
+package com.team01.billage.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@AllArgsConstructor
+@Getter
+public enum ErrorCode {
+    /* 400 BAD_REQUEST : 잘못된 요청 */
+    PRODUCT_MODIFICATION_NOT_ALLOWED(HttpStatus.BAD_REQUEST, "현재 대여 중인 상품은 수정/삭제할 수 없습니다."),
+
+    /* 401 UNAUTHORIZED : 인증되지 않은 사용자 */
+
+    /* 403 FORBIDDEN : 권한이 없는 사용자 */
+
+    /* 404 NOT_FOUND : Resource를 찾을 수 없음 */
+    CATEGORY_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 카테고리를 찾을 수 없습니다."),
+    PRODUCT_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 상품을 찾을 수 없습니다.");
+
+    /* 409 : CONFLICT : Resource의 현재 상태와 충돌. 보통 중복된 데이터 존재, 조건을 만족하지 못함 */
+
+    /* 410 : GONE : 리소스가 더 이상 유효하지 않음 */
+
+    /* 500 INTERNAL_SERVER_ERROR : 서버 내부 에러 */
+
+    private final HttpStatus httpStatus;
+    private final String message;
+}
