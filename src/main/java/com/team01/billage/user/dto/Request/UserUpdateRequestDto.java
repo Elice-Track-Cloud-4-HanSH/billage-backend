50b1eea08bf498fbd78ffe40d60baab666f73518
dbsghwns <dbsghwns1209@gmail.com>
2024-11-25 01:26:38 +0900

    <fix>: [7] BE MR Request 수정

--- src/main/java/com/team01/billage/user/dto/Request/UserUpdateRequestDto.java
+++ src/main/java/com/team01/billage/user/dto/Request/UserUpdateRequestDto.java
@@ -1,5 +1,8 @@
 package com.team01.billage.user.dto.Request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,9 +13,18 @@ import lombok.Setter;
 @NoArgsConstructor
 public class UserUpdateRequestDto {
 
+    @NotBlank(message = "닉네임은 필수 입력값입니다.")
+    @Size(max = 15, message = "닉네임은 15자 이하여야 합니다.")
     private String nickname;
+
+    @NotBlank(message = "비밀번호는 필수 입력값입니다.")
+    @Size(min = 8, max = 20, message = "비밀번호는 8자 이상 20자 이하여야 합니다.")
     private String password;
+
     private String imageUrl;
+
+    @NotBlank(message = "자기소개는 필수 입력값입니다.")
+    @Size(min = 5, max = 50, message = "자기소개는 5자 이상 50자 이하여야 합니다.")
     private String description;
 
     @Builder
