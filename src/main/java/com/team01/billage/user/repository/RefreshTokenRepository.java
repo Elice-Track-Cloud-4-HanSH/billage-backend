628f7e0bec47ee91a87e716e754fc9def6bfa0f0
Yoonhojoon <dbsghwns1209@gmail.com>
2024-11-28 14:08:38 +0900

    fix: #13 - redis RefreshToken 추가

--- src/main/java/com/team01/billage/user/repository/RefreshTokenRepository.java
+++ src/main/java/com/team01/billage/user/repository/RefreshTokenRepository.java
@@ -1,13 +1,14 @@
-//package com.team01.billage.user.repository;
-//
-//import com.nimbusds.oauth2.sdk.token.RefreshToken;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.stereotype.Repository;
-//
-//import java.util.Optional;
-//
-//@Repository
-//public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
-//    Optional<RefreshToken> findByEmail(String email);
-//    Optional<RefreshToken> findByRefreshToken(String refreshToken);
-//}
+package com.team01.billage.user.repository;
+
+import com.nimbusds.oauth2.sdk.token.RefreshToken;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RefreshTokenRepository extends CrudRepository<RefreshToken, String> {
+    Optional<RefreshToken> findByUserId(Long userId);    // String -> Long으로 변경, 메서드명 카멜케이스로 수정
+    Optional<RefreshToken> findByRefreshToken(String refreshToken);
+}
