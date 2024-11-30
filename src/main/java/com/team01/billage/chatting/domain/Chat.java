82110ca3209e4b46fcd67bef811287adb594235e
PromotezCitizen <gkstkdgus821@gmail.com>
2024-11-30 14:00:46 +0900

    modify: 채팅의 최대 길이를 1000자로 설정

--- src/main/java/com/team01/billage/chatting/domain/Chat.java
+++ src/main/java/com/team01/billage/chatting/domain/Chat.java
@@ -32,7 +32,7 @@ public class Chat {
     @JoinColumn(name = "sender_id")
     private TestUser sender;
 
-    @Column(nullable = false)
+    @Column(nullable = false, length=1000)
     private String message;
 
     @Column(nullable = false)
