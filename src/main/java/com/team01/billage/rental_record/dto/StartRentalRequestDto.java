294a8a547e0f152f249114c25bc4f6569c3ffa2c
[CLOUD]나용진 <yjnaga@naver.com>
2024-12-02 14:22:15 +0900

    상품 후기 생성, 조회 테스트 및 수정

--- src/main/java/com/team01/billage/rental_record/dto/StartRentalRequestDto.java
+++ src/main/java/com/team01/billage/rental_record/dto/StartRentalRequestDto.java
@@ -1,18 +1,18 @@
 package com.team01.billage.rental_record.dto;
 
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import java.time.LocalDate;
 import lombok.Getter;
 
 @Getter
 public class StartRentalRequestDto {
 
-    @NotBlank(message = "거래 상대방을 선택해주세요.")
+    @NotNull
     private long id;
 
-    @NotBlank(message = "대여 시작일을 선택해주세요.")
+    @NotNull
     private LocalDate startDate;
 
-    @NotBlank(message = "반납 예정일을 선택해주세요.")
-    private LocalDate expectedRentalDate;
+    @NotNull
+    private LocalDate expectedReturnDate;
 }
