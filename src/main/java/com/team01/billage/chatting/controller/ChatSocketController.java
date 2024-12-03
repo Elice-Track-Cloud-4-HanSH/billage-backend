ce2a649a85ec359281505b65211c4063bd5b30eb
PromotezCitizen <gkstkdgus821@gmail.com>
2024-12-03 21:34:23 +0900

    refactoring: ChatController에 종속된 서비스로직 분리

--- src/main/java/com/team01/billage/chatting/controller/ChatSocketController.java
+++ src/main/java/com/team01/billage/chatting/controller/ChatSocketController.java
@@ -1,32 +1,21 @@
 package com.team01.billage.chatting.controller;
 
-import com.team01.billage.chatting.domain.Chat;
-import com.team01.billage.chatting.domain.ChatRoom;
 import com.team01.billage.chatting.dto.ChatMessage;
 import com.team01.billage.chatting.dto.ChatResponseDto;
-import com.team01.billage.chatting.repository.ChatRepository;
-import com.team01.billage.chatting.repository.ChatRoomRepository;
-import com.team01.billage.exception.CustomException;
-import com.team01.billage.exception.ErrorCode;
+import com.team01.billage.chatting.service.ChatSocketService;
 import com.team01.billage.user.domain.Users;
-import com.team01.billage.user.repository.UserRepository;
 import com.team01.billage.utils.DetermineUser;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
-import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.stereotype.Controller;
 
 @Controller
 @RequiredArgsConstructor
-public class ChatController {
+public class ChatSocketController {
 
-    private final ChatRoomRepository chatRoomRepository;
-    private final ChatRepository chatRepository;
-
-    private final UserRepository userRepository;
-
+private final ChatSocketService chatSocketService;
     private final DetermineUser determineUser;
 
     @MessageMapping("/chat/{chatroomId}")
@@ -38,20 +27,12 @@ public class ChatController {
     ) {
         Users sender = determineUser.determineUser(principal.getName());
 
-        ChatRoom chatRoom = chatRoomRepository.findById(chatroomId).orElseThrow(() -> new CustomException(ErrorCode.CHATROOM_NOT_FOUND));
-        Chat chat = Chat.builder()
-                .chatRoom(chatRoom)
-                .sender(sender)
-                .message(message.getMessage())
-                .build();
-
-        chatRepository.save(chat);
-
-        return chat.toChatResponse(sender.getId());
+        ChatResponseDto responseDto = chatSocketService.insertChat(chatroomId, sender, message);
+        return responseDto;
     }
 
     @MessageMapping("/chat/chatting/{chatId}")
     public void ack(@DestinationVariable Long chatId, java.security.Principal principal) {
-        chatRepository.markAsRead(chatId);
+        chatSocketService.markAsRead(chatId);
     }
 }
