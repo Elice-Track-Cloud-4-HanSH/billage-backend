f7e0fa4ea1d430c9dc1f3eca1a3614f1221d1fbe
[CLOUD4] 허수빈 <anmedi3623@naver.com>
2024-12-04 21:33:33 +0900

    refactor: 안쓰는 메서드 삭제

--- src/main/java/com/team01/billage/product/repository/ProductRepository.java
+++ src/main/java/com/team01/billage/product/repository/ProductRepository.java
@@ -1,7 +1,6 @@
 package com.team01.billage.product.repository;
 
 import com.team01.billage.product.domain.Product;
-import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -10,7 +9,5 @@ import org.springframework.stereotype.Repository;
 public interface ProductRepository extends JpaRepository<Product, Long>, CustomProductRepository {
 
     Optional<Product> findByIdAndDeletedAtIsNull(Long id);
-
-    List<Product> findAllByDeletedAtIsNull();
 
 }
