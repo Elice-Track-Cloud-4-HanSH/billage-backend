4407dfa110644cfb76acce98a2572dcfba69f30c
[CLOUD4] 백승주 <bsj5967@naver.com>
2024-12-08 13:33:30 +0900

    fix : #52 get neighborarea api 추가

--- src/main/java/com/team01/billage/map/repository/NeighborAreaRepository.java
+++ src/main/java/com/team01/billage/map/repository/NeighborAreaRepository.java
@@ -4,6 +4,8 @@ import com.team01.billage.map.domain.NeighborArea;
 import com.team01.billage.map.domain.EmdArea;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -13,5 +15,14 @@ public interface NeighborAreaRepository extends JpaRepository<NeighborArea, Long
     // 특정 행정동 코드로 주변 지역 검색
     List<NeighborArea> findByEmdArea(EmdArea emdArea);
     Optional<NeighborArea> findByEmdAreaAndDepth(EmdArea emdArea, int depth);
+
+    @Query(value = """
+SELECT ST_AsGeoJSON(geom) AS geomGeoJson
+FROM neighbor_area
+WHERE emd_cd = :emdCd
+  AND depth = :depth
+LIMIT 1
+""", nativeQuery = true)
+    Object findNeighborAreaWithGeoJsonByEmdCdAndDepth(@Param("emdCd") Long emdCd, @Param("depth") int depth);
 
 }
