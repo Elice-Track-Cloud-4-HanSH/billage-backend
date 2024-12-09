fa3e61751530677047b3944307ba54b27483a12f
PromotezCitizen <gkstkdgus821@gmail.com>
2024-12-09 14:41:38 +0900

    fix: QueryDSL의 반환값을 DTO로 수정

--- src/main/java/com/team01/billage/chatting/repository/ChatQueryDSL.java
+++ src/main/java/com/team01/billage/chatting/repository/ChatQueryDSL.java
@@ -1,11 +1,14 @@
 package com.team01.billage.chatting.repository;
 
 import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.Projections;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.team01.billage.chatting.domain.Chat;
 import com.team01.billage.chatting.domain.QChat;
 import com.team01.billage.chatting.domain.QChatRoom;
+import com.team01.billage.chatting.dto.querydsl.ChatWithSenderDTO;
+import com.team01.billage.chatting.dto.querydsl.ChatroomWithRecentChatDTO;
 import com.team01.billage.user.domain.QUsers;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
@@ -18,28 +21,35 @@ import java.util.List;
 public class ChatQueryDSL {
     private final JPAQueryFactory queryFactory;
 
-    public List<Chat> getPagenatedChatsInChatroom(Long chatroomId, Long chatId, Long userId, Pageable pageable) {
+    public List<ChatWithSenderDTO> getPagenatedChatsInChatroom(Long chatroomId, Long chatId, Long userId, Pageable pageable) {
         BooleanBuilder builder = new BooleanBuilder();
 
-        QChatRoom chatroom = QChatRoom.chatRoom;
-        QChat chat = QChat.chat;
-        QUsers seller = QUsers.users;
-        QUsers buyer = new QUsers("buyer");
+        QChatRoom qChatroom = QChatRoom.chatRoom;
+        QChat qChat = QChat.chat;
+        QUsers qSeller = QUsers.users;
+        QUsers qBuyer = new QUsers("buyer");
 
-        builder.and(chat.chatRoom.id.eq(chatroomId));
-        builder.and(chat.id.lt(chatId));
+        BooleanExpression sellerCondition = qChatroom.seller.id.eq(userId).and(qChatroom.sellerExitAt.isNull());
+        BooleanExpression buyerCondition = qChatroom.buyer.id.eq(userId).and(qChatroom.buyerExitAt.isNull());
 
-        BooleanExpression sellerCondition = chatroom.seller.id.eq(userId).and(chatroom.sellerExitAt.isNull());
-        BooleanExpression buyerCondition = chatroom.buyer.id.eq(userId).and(chatroom.buyerExitAt.isNull());
-
-        builder.and(sellerCondition.or(buyerCondition));
-
-        List<Chat> chats = queryFactory.selectFrom(chat)
-                .join(chat.chatRoom, chatroom)
-                .leftJoin(chatroom.seller, seller)
-                .leftJoin(chatroom.buyer, buyer)
-                .where(builder)
-                .orderBy(chat.createdAt.desc())
+        List<ChatWithSenderDTO> chats = queryFactory
+                .select(Projections.constructor(
+                        ChatWithSenderDTO.class,
+                        qChat.id,
+                        Projections.constructor(ChatWithSenderDTO.User.class, qChat.sender.id, qChat.sender.nickname),
+                        Projections.constructor(ChatWithSenderDTO.Message.class, qChat.message, qChat.createdAt),
+                        qChat.isRead
+                ))
+                .from(qChat)
+                .join(qChat.chatRoom, qChatroom)
+                .leftJoin(qChatroom.seller, qSeller)
+                .leftJoin(qChatroom.buyer, qBuyer)
+                .where(
+                        qChat.chatRoom.id.eq(chatroomId),
+                        builder.and(qChat.id.lt(chatId)),
+                        sellerCondition.or(buyerCondition)
+                )
+                .orderBy(qChat.createdAt.desc())
                 .offset(pageable.getOffset())
                 .limit(pageable.getPageSize())
                 .fetch();
